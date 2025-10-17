231e65ea72debabf03ef2c50d63d5edad54125c8
Md Sadique <mdsadique47@gmail.com>
2025-10-17 23:22:04 +0530

    Stop tracking webclientid file

--- core/auth/src/main/java/com/reyaz/core/auth/domain/repository/WebClientId.kt
+++
@@ -1,6 +0,0 @@
-package com.reyaz.core.auth.domain.repository
-
-object WebClientId {
-    val API_KEY="759891781981-82uprgakln0njp8b4me3m08ijni75v0o.apps.googleusercontent.com"
-}
-//this is web client id auto created by google ,i can also my own
